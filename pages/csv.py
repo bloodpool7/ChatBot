f16ffd5abb4323d2ade85bc97d178d4a1f397e95
Rishabh Goel <rishabhgoel@Rishabhs-Air.localdomain>
2023-08-09 12:00:49 -0700

    Added csv functionality

--- pages/csv.py
+++ pages/csv.py
@@ -16,4 +16,14 @@ file = st.file_uploader("Upload csv to complete", type = "csv")
 if file:
     df = pd.read_csv(file)
 
+    answers = []
+    chat_history = []
+    for question in df["question"]:
+        result = utils.query_documents(question, namespace, openai_api_key = openai_api_key, pinecone_api_key = pinecone_api_key, pinecone_env=pinecone_env, index_name = "test", chat_history = chat_history) 
+        answers.append(result['answer'])
+        chat_history.append((question, result['answer']))
     
+    df["answer"] = answers
+    st.dataframe(df, width = 500, height = 500)
+    st.download_button("Download csv", data = df.to_csv(), file_name = file.name[:-4] + "_completed.csv", mime = "text/csv")
+
