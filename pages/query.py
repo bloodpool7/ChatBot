118734fcface02bac69e72c2f750c908a0065533
Rishabh Goel <rishabhgoel@Rishabhs-Air.localdomain>
2023-08-20 12:36:01 -0700

    minor changes

--- pages/query.py
+++ pages/query.py
@@ -11,15 +11,15 @@ chat_history = []
 
 st.set_page_config(page_title="Query PDFs")
 
+pdfs = st.multiselect("Choose pdfs to search", st.session_state['added_pdfs'])
+
 st.write("Enter your query here:")
 query = st.text_input("Query")
 
-pdfs = st.multiselect("Choose pdfs to search", st.session_state['added_pdfs'])
-
 if query:
     if pdfs:
-        result = utils.query_documents(query, namespace, filter = {"$in":pdfs}, openai_api_key = openai_api_key, pinecone_api_key = pinecone_api_key, pinecone_env=pinecone_env, index_name = "test", chat_history = chat_history) 
+        result = utils.query_documents(query, namespace, filter = {'title': {"$in":pdfs}}, openai_api_key = openai_api_key, pinecone_api_key = pinecone_api_key, pinecone_env=pinecone_env, index_name = "test", chat_history = chat_history) 
     else:
         result = utils.query_documents(query, namespace, openai_api_key = openai_api_key, pinecone_api_key = pinecone_api_key, pinecone_env=pinecone_env, index_name = "test", chat_history = chat_history)
     chat_history.append((query, result['answer']))
-    st.write(result['answer'])
+    st.code(result['answer'], language = 'markdown')
