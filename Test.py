df66d1e159db025146858e9fee4904d509b4d626
Rishabh Goel <rishabhgoel@Rishabhs-Air.localdomain>
2023-09-30 00:18:10 -0700

    Improvements

--- Test.py
+++ Test.py
@@ -1,53 +1,11 @@
-import pinecone
-import os
-import openai
-import PyPDF2
+import seaborn as sns
+import numpy as np
+import matplotlib.pyplot as plt
 
-def add_to_database(ailment):
-    PINECONE_ENV = os.getenv("PINECONE_ENV")
-    PINECONE_API_KEY = os.getenv("PINECONE_API_KEY")
+matrix = 5 * np.eye(5)
 
-    pinecone.init(
-            api_key=PINECONE_API_KEY,
-            environment=PINECONE_ENV
-    )
-    index = pinecone.Index("test")
+sns.heatmap(matrix, annot=True, cmap="Blues", xticklabels=["number 1", "number 2", "number 3", "number 4", "number 5"], yticklabels=["number 1", "number 2", "number 3", "number 4", "number 5"])
 
-    result = openai.ChatCompletion.create(
-        model = "gpt-3.5-turbo", 
-        messages = [{"role": "user", "content" : "What are commonly observable symptoms of " + ailment + " ?"}]
-    )["choices"][0]['message']['content']
+plt.savefig("filename.pdf", dpi=300)
 
-    result_embedding = openai.Embedding.create(
-        input=result,
-        model="text-embedding-ada-002"
-    )['data'][0]['embedding']
-
-    index.upsert([(ailment, result_embedding)], namespace = "sideproject")
-
-def classify(query):
-    PINECONE_ENV = os.getenv("PINECONE_ENV")
-    PINECONE_API_KEY = os.getenv("PINECONE_API_KEY")
-
-    pinecone.init(
-            api_key=PINECONE_API_KEY,
-            environment=PINECONE_ENV
-    )
-    index = pinecone.Index("test")
-
-    outputs = index.query(
-        vector = openai.Embedding.create(input = query, model = "text-embedding-ada-002")['data'][0]['embedding'], 
-        top_k = 5,
-        namespace = 'sideproject',
-        include_values = True
-    )
-
-    return outputs
-
-def get_text_from_pdf(pdf):
-    reader = PyPDF2.PdfReader(pdf)
-    extracted_text = ""
-
-    for i in range(len(reader.pages)):
-        page = reader.pages[i]
-        extracted_text += page.extract_text()    
+plt.show()
